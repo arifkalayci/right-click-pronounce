52b2e7c6a76b04bb94108b608d1c7e7072135558
lunrfarsde <lunrfarsde@gmail.com>
2013-05-26 13:32:08 +0300

    Initial push

---
+++ hear.js
@@ -0,0 +1,26 @@
+function onClick(info, tab) {
+	$.ajax({
+		url: "http://www.tfd.com/" + info.selectionText,
+	}).done(
+		function(html) {
+			var re = /play_w2\(\"(\S*?)\"\)/
+			var match = re.exec(html);
+			console.log(match[1]);
+			
+			audio = document.createElement("audio");
+			audio["src"] = "http://img.tfd.com/hm/mp3/" + match[1] + ".mp3";
+			document.body.appendChild(audio);
+			audio.play();
+		}
+	).fail(
+		function(response) {
+			console.log("fail:" + response);
+		}
+	);
+}	
+
+chrome.contextMenus.create({
+	"title": "Hear this word", 
+	"contexts": ["selection"], 
+	"onclick": onClick
+});
