cd9d8d87c90b9ec8bed57e18cc492c200ac05788
Arif Kalayci <arifkalayci@gmail.com>
2020-01-22 23:23:55 +0700

    Convert indentation to spaces

--- right-click-pronounce.js
+++ right-click-pronounce.js
@@ -1,26 +1,26 @@
 function onClick(info, tab) {
-	$.ajax({
-		url: "http://www.tfd.com/" + info.selectionText,
-	}).done(
-		function(html) {
-			var re = /play_w2\(\"(\S*?)\"\)/
-			var match = re.exec(html);
-			console.log(match[1]);
+  $.ajax({
+    url: "http://www.tfd.com/" + info.selectionText,
+  }).done(
+    function(html) {
+      var re = /play_w2\(\"(\S*?)\"\)/
+      var match = re.exec(html);
+      console.log(match[1]);
 
-			audio = document.createElement("audio");
-			audio["src"] = "http://img.tfd.com/hm/mp3/" + match[1] + ".mp3";
-			document.body.appendChild(audio);
-			audio.play();
-		}
-	).fail(
-		function(response) {
-			console.log("fail:" + response);
-		}
-	);
+      audio = document.createElement("audio");
+      audio["src"] = "http://img.tfd.com/hm/mp3/" + match[1] + ".mp3";
+      document.body.appendChild(audio);
+      audio.play();
+    }
+  ).fail(
+    function(response) {
+      console.log("fail:" + response);
+    }
+  );
 }
 
 chrome.contextMenus.create({
-	"title": "Pronounce this word",
-	"contexts": ["selection"],
-	"onclick": onClick
+  "title": "Pronounce this word",
+  "contexts": ["selection"],
+  "onclick": onClick
 });
